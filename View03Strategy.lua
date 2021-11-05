06432a19a9edc5dc9d9b7ccbcf95326b431e7b88
Steffen Hoffmann <shoffmann@palettecad.com>
2021-11-05 13:38:20 +0100

    started with strategy tab;

--- View03Strategy.lua
+++ View03Strategy.lua
@@ -2,11 +2,25 @@
 --*                               View03Strategy.lua                              *
 --* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
 local View = {}
+local Controller = require("Controller")
 
 ------------------------------------------------------------------------------------
 --- Main()
 ------------------------------------------------------------------------------------
 ---
+local tTranslation = Controller.Read("translation")
+local tOptions = Controller.Read("options")
+if tOptions.sFilename ~= nil then
+  print("if")
+  local tData = Controller.Read(tOptions.sFilename)
+else
+  print("else")
+  local tData = {}
+end
+-- labels
+local lblDriver = iup.label {
+
+}
 View = iup.vbox {}
 
 return View
