3829fb50b4741d75cb34a71fce90456d7669dd38
Steffen Hoffmann <shoffmann@palettecad.com>
2021-11-05 17:01:08 +0100

    before spltting files;

--- View03Strategy.lua
+++ View03Strategy.lua
@@ -8,19 +8,106 @@ local Controller = require("Controller")
 --- Main()
 ------------------------------------------------------------------------------------
 ---
-local tTranslation = Controller.Read("translation")
 local tOptions = Controller.Read("options")
+local nLanguage = tOptions.language.value
+
+local tTranslation = Controller.Read("translation")
+local valTranslation = tTranslation.languages[nLanguage]
+
 if tOptions.sFilename ~= nil then
-  print("if")
   local tData = Controller.Read(tOptions.sFilename)
 else
-  print("else")
   local tData = {}
 end
--- labels
-local lblDriver = iup.label {
 
+-- labels
+local lblRacestart = iup.label {title = valTranslation.RACE_START}
+local lblHours = iup.label {title = valTranslation.HOUR}
+local lblMinutes = iup.label {title = valTranslation.MINUTE}
+
+local lblRaceDuration = iup.label {title = valTranslation.DURATION}
+local lblDurationHours = iup.label {title = valTranslation.HOURS}
+local lblDurationMinutes = iup.label {title = valTranslation.MINUTES}
+
+local lblStintLength = iup.label {title = valTranslation.STINT}
+local lblStintHours = iup.label {title = valTranslation.HOURS}
+local lblStintMinutes = iup.label {title = valTranslation.MINUTES}
+
+-- textboxes
+local txtRacestartH = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
 }
-View = iup.vbox {}
+local txtRacestartM = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
+}
+local txtDurationH = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
+}
+local txtDurationM = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
+}
+local txtStintH = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
+}
+local txtStintM = iup.text {
+  value = 00,
+  size = 15,
+  alignment = "acenter"
+}
+
+View = iup.hbox {
+  iup.vbox {
+    lblRacestart,
+    iup.hbox {
+      iup.vbox {
+        lblHours,
+        txtRacestartH,
+      },
+      iup.vbox {
+        lblMinutes,
+        txtRacestartM
+      },
+    },
+  },
+  iup.vbox {
+    lblRaceDuration,
+    iup.hbox {
+      iup.vbox {
+        lblDurationHours,
+        txtDurationH,
+      },
+        iup.vbox {
+        lblDurationMinutes,
+        txtDurationM
+      },
+    },
+  },
+  iup.vbox {
+    lblStintLength,
+    iup.hbox {
+      iup.vbox {
+        lblStintHours,
+        txtStintH
+      },
+      iup.vbox {
+        lblStintMinutes,
+        txtStintM
+      }
+    }
+  },
+  alignment = "acenter",
+  gap = "10",
+  margin = "10x10",
+}
 
 return View
